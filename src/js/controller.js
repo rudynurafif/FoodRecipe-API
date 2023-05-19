72b8ad4421b599cf349972927c1818d257ccfeda
rudynurafif <rudynurafif@gmail.com>
2023-05-20 04:42:49 +0700

    Editing welcome message

--- src/js/controller.js
+++ src/js/controller.js
@@ -133,7 +133,7 @@ const controlAddRecipe = async function (newRecipe) {
 };
 
 const newFeature = function () {
-  console.log('Welcome to the application');
+  console.log('Welcome to the application!');
 };
 
 const init = function () {
